59c5e656c0e44df0b45f2e5d254809d7745c21df
zhangjie <jwczjzl@yeah.net>
2017-03-25 17:50:50 +0800

    使用下蛋例子说明联合类型,双方只能访问共有的方法or属性

--- TypeScript/Untitled-10.ts
+++ TypeScript/Untitled-10.ts
@@ -1,3 +1,26 @@
 /**
  * 交叉类型:交叉类型是将多个类型合并为一个类型
  */
+
+/**
+ * 联合类型 书写格式 = >   " string | number "
+ * 如果一个实例是联合类型,那么我们只能访问他们共有的属性(方法)
+ */
+
+interface Fish{
+    swim():void;
+    layEggs():void;
+}
+interface Bird{
+    fly():void;
+    layEggs():void;
+}
+
+function animal_common():Bird | Fish{
+    //  ....
+}
+
+let pet = animal_common();
+pet.layEggs(); // 共有的 " 下蛋 "
+pet.swim(); // Property 'swim' does not exist on type 'Fish | Bird'.Property 'swim' does not exist on type 'Bird'.
+
